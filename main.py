d7203f3b2d19997000ebd9c68308158ca311b79a
Ekaterina Ivanova <katenika158@gmail.com>
2020-06-01 23:57:49 +0300

    Add translator

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.bot_longpoll import VkBotLongPoll
 from vk_api.utils import get_random_id
 import requests
-from random import randint as rnd
 from vk.exceptions import VkAPIError
 import json
-from vk_api import bot_longpoll
+from googletrans import Translator
 
 token = '1aa70f3daf3610a84669b33954f8968ee7d6ff52cd383df18479ecee767d8ad60455a71845c9ddc5c26e3'
 
-session = vk_api.VkApi(token=token)
+session = vk_api.VkApi(token=token, api_version='5.107')
 vkapi = session.get_api()
 longpoll = VkBotLongPoll(session, group_id=195861998, wait=25)
 
-# session = vk_api.auth(access_token=token)
-# vkapi = vk.API(session, v="5.107")
-
 info = vkapi.groups.getLongPollServer(group_id=195861998)
-server = info['server']
-key = info['key']
-ts = info['ts']
+server, key, ts = info['server'], info['key'], info['ts']
 
 vkapi.groups.setLongPollSettings(group_id=195861998, message_new=1)
 
 with open('keyboard.json', 'r', encoding='utf-8') as f:
     keyboard = json.dumps(json.load(f))
 
+translator = Translator()
+
 while True:
 
-    response = requests.get('{server}?act=a_check&key={key}&ts={ts}&wait=25'.format(server=server,
+    long_poll_response = requests.get('{server}?act=a_check&key={key}&ts={ts}&wait=25'.format(server=server,
                                                                                     key=key, ts=ts)).json()
-    if 'updates' not in response:
+    if 'updates' not in long_poll_response:
         continue
 
-    msg = ''
-    user_id = ''
+    msg, user_id = '', ''
 
     try:
-        for el in response['updates']:
+        for el in long_poll_response['updates']:
             user_id = el['object']['user_id']
 
             if el['type'] == 'message_new':
@@ -46,13 +41,22 @@ while True:
                         vkapi.messages.send(user_id=user_id, random_id=get_random_id(), peer_id=user_id,
                                             message='Привет, я могу рассказать тебе о погоде!')
 
-                        vkapi.messages.send(user_id=user_id, random_id=rnd(1, 2 ** 63), peer_id=user_id,
+                        result = translator.translate('Привет, я могу рассказать тебе о погоде!', src='ru', dest='en')
+                        vkapi.messages.send(user_id=user_id, random_id=get_random_id(), peer_id=user_id,
+                                            message=result.text)
+                        vkapi.messages.sendп(user_id=user_id, random_id=get_random_id(), peer_id=user_id,
                                             message="Выберите действие",
-                                keyboard=keyboard)
+                                            keyboard=keyboard)
+                    # elif el['object']['body'] == 'Хочу узнать погоду':
+
+                        # result = translator.translate(el['object']['body'], src='ru', dest='en')
+                        # vkapi.messages.send(user_id=user_id, random_id=get_random_id(), peer_id=user_id,
+                        #                     message=result.text)
     except VkAPIError:
         continue
 
     print(requests.get('{server}?act=a_check&key={key}&ts={ts}&wait=25'.format(server=server,
                                                                                key=key, ts=ts)).json())
-    ts = str(int(ts) + 1)
+
+    ts = long_poll_response['ts']
 
